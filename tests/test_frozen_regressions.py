2bf8dffa3a439faabdfef6dcedd4e394f6668662
ryotamatsuki <60263857+ryotamatsuki@users.noreply.github.com>
2026-09-23 19:24:36 +0900

    repro: add post-Astra source profit and endpoint tests

--- tests/test_frozen_regressions.py
+++ tests/test_frozen_regressions.py
@@ -8,6 +8,13 @@ from uniform_game_cleanroom import (
     share_a_direct,
     share_a_piecewise,
     source_uniform_prices,
+    source_eq15_switch_cs,
+    source_eq16_printed_cs,
+    source_uniform_profile_profits,
+    weak_hbp_source_profits,
+    hbp_strong_profile_unrestricted,
+    hbp_profits_direct,
+    hbp_cs_direct,
     x_h_condition,
 )
 
@@ -50,6 +57,37 @@ class FrozenRegressionTests(unittest.TestCase):
         a, b = source_uniform_prices(s)
         best_b, _ = direct_best_response_candidates("B", a, x, s)
         self.assertGreater(best_b, profit_b(a, b, x, s))
+
+
+    def test_eq15_source_fidelity_and_eq16_sign(self):
+        s, x = F(1, 10), F(7, 10)
+        self.assertEqual(source_eq15_switch_cs(x, s), F(221, 720))
+        self.assertEqual(source_eq16_printed_cs(x, s), F(-221, 720))
+        self.assertEqual(source_eq16_printed_cs(x, s), -source_eq15_switch_cs(x, s))
+
+    def test_weak_profit_no_switch_branch(self):
+        s, x = F(1, 2), F(51, 100)
+        _, pi_b_d = weak_hbp_source_profits(x, s)
+        _, pi_b_u = source_uniform_profile_profits(x, s)
+        self.assertEqual(pi_b_d, F(3221, 9000))
+        self.assertEqual(pi_b_u, F(49, 120))
+        self.assertEqual(pi_b_d - pi_b_u, F(-227, 4500))
+
+    def test_strong_profit_does_not_inherit_weak_ranking(self):
+        s, x = F(9, 10), F(19, 20)
+        profile = hbp_strong_profile_unrestricted(x, s, F(0))
+        _, pi_b_d = hbp_profits_direct(*profile, x, s)
+        _, pi_b_u = source_uniform_profile_profits(x, s)
+        self.assertEqual(pi_b_d - pi_b_u, F(41, 900))
+
+    def test_empty_segment_prices_are_indeterminate_at_x1(self):
+        s, x = F(1, 2), F(1)
+        p_a = (2 * x + 1 + s) / 3
+        q_b = (4 * x - 1 - s) / 3
+        base = (p_a, F(0), 1 + s, q_b)
+        alt = (p_a, F(7), F(9), q_b)
+        self.assertEqual(hbp_profits_direct(*base, x, s), hbp_profits_direct(*alt, x, s))
+        self.assertEqual(hbp_cs_direct(*base, x, s), hbp_cs_direct(*alt, x, s))
 
 
 if __name__ == "__main__":
